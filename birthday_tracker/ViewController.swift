a17010b2b9efe9a34a0c005c5d167947a0c66b2c
Jahnvi <jpate201@uic.edu>
2018-06-13 12:19:33 -0500

    Initial Commit

---
+++ birthday_tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  birthday_tracker
+//
+//  Created by Janvi on 6/13/18.
+//  Copyright © 2018 Jahnvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
